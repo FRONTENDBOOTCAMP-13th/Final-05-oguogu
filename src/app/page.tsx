b78ce8c7224156db6ceee904f733ffd582c94169
최영준 <jjmullan24@gmail.com>
2025-07-18 11:19:13 +0900

    🩹 fix: 루트 layout 구조 개선 - {children} 을 div 로 감싸고, 래퍼 요소에 반응형 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,36 @@ import { ImageCategory } from '@/components/layouts/Category/Category';
 import Title from '@/components/elements/CommonTitleItem/Title';
 import ProductLinkItem from '@/components/elements/ProductLink/ProductLink';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default async function Home() {
   return (
     <>
-      <div className="min-w-[320px] max-w-[768px] mx-auto relative bg-oguogu-white z-50">
-        {/* 헤더가 고정되지 않고 흔들리는 이슈 해결 필요 */}
-        <Header cartItemCount={100} />
-        <Navigation />
-        <SlideBanner />
-        <ImageCategory />
-        <main className="flex flex-col">
-          <Title title="온 가족이 함께 즐기는 텃밭 체험" content="7월 한 달 간 체험 상품 20% 할인!" type="experience" />
-          <ProductLinkItem keywordParams="" typeParams="" linkTitle="전체 체험 상품" />
-          <Title title="톡! 터지는 옥수수의 반전 매력" content="7월 제철 채소 옥수수🌽" type="crop" />
-          <ProductLinkItem keywordParams="옥수수" linkTitle="옥수수" />
-          {/* <Title title="톡! 터지는 옥수수의 반전 매력" content="7월 제철 채소 옥수수🌽" type="gardening" /> */}
-        </main>
-        <footer>
-          <Link href="/product/crop/veggie/1/detail">상세페이지로 이동하기 (임시)</Link>
-        </footer>
-      </div>
-      <Image
+      <Header cartItemCount={100} />
+      <Navigation />
+      <SlideBanner />
+      <ImageCategory />
+      <main className="flex flex-col">
+        <Title title="온 가족이 함께 즐기는 텃밭 체험" content="7월 한 달 간 체험 상품 20% 할인!" type="experience" />
+        <ProductLinkItem
+          keywordParams=""
+          typeParams=""
+          link="/product/experience"
+          linkTitle="전체 체험 상품"
+          subTxt="보러 가기"
+        />
+        <Title title="톡! 터지는 옥수수의 반전 매력" content="7월 제철 채소 옥수수🌽" type="crop" />
+        <ProductLinkItem
+          link="/search/result?keyword=옥수수"
+          keywordParams="옥수수"
+          linkTitle="옥수수"
+          subTxt="구매하러 가기"
+        />
+        {/* <Title title="톡! 터지는 옥수수의 반전 매력" content="7월 제철 채소 옥수수🌽" type="gardening" /> */}
+      </main>
+      <footer>
+        <Link href="/product/crop/veggie/1/detail">상세페이지로 이동하기 (임시)</Link>
+      </footer>
+      {/* <Image
         src="/images/image.png"
         width={180}
         height={180}
@@ -40,7 +47,7 @@ export default async function Home() {
         height={180}
         alt="ogu"
         className="hidden fixed bottom-2/7 left-1/2 translate-x-[420px] mobile-max:block"
-      />
+      /> */}
     </>
   );
 }
