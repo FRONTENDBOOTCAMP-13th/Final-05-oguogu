bc1c8d29d2682a4dd3c0965a459608fb1e1357fe
최영준 <jjmullan24@gmail.com>
2025-07-18 21:59:42 +0900

    🩹 fix: Pathname 조건문 사용을 위해 컴포넌트화 및 컴포넌트 태그로 수정

--- src/app/(exploring)/search/result/[_id]/layout.tsx
+++ src/app/(exploring)/search/result/[_id]/layout.tsx
@@ -1,15 +1,11 @@
-import { TextCategoryItem } from '@/components/elements/CategoryItem/CategoryItem';
+import { TextCategoryForDetailPage } from '@/components/layouts/Category/Category';
 import CategoryHeader from '@/components/layouts/Header/CategoryHeader';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-w-[320px] max-w-[768px] mx-auto relative bg-oguogu-white z-50">
       <CategoryHeader />
-      <nav className="flex secondHeader">
-        <TextCategoryItem type="search" _id={1} params="detail" title="상품 정보" isClick={true} />
-        <TextCategoryItem type="search" _id={1} params="review" title="리뷰(1,280)" isClick={false} />
-        <TextCategoryItem type="search" _id={1} params="qna" title="문의(9,999)" isClick={false} />
-      </nav>
+      <TextCategoryForDetailPage />
       {children}
     </div>
   );
