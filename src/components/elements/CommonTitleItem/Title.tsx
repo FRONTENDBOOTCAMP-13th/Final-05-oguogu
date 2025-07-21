4c95a06e3a692b37e32b58bb5185f40080f6bf4a
최영준 <jjmullan24@gmail.com>
2025-07-21 11:43:37 +0900

    💄 style: Title 컴포넌트 상품 상세 페이지 재사용을 위한 스타일 재조정 - 768px 이상이어도 왼쪽 정렬이 되도록 수정

--- src/components/elements/CommonTitleItem/Title.tsx
+++ src/components/elements/CommonTitleItem/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title({ title = '제목', description = '부제목' }: { title: string; description: string }) {
   return (
-    <h2 className="mobile-max:text-center">
+    <h2>
       <p className="text-xl text-oguogu-black mobile-max:text-2xl">{title}</p>
       <p className="text-sm text-oguogu-gray-4 mobile-max:text-base">{description}</p>
     </h2>
