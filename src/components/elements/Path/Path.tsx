97352e77f38c4be1922bb4463273caf9c653cc65
최영준 <jjmullan24@gmail.com>
2025-07-21 17:45:06 +0900

    🐛 bugfix: Path 코드 구조 개선 - (기존) document.referrer.includes(...) - (수정) usePathname 훅 사용 Ref #125

--- src/components/elements/Path/Path.tsx
+++ src/components/elements/Path/Path.tsx
@@ -1,25 +1,26 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-// CHECKLIST
-// [ ] params="crops" subParams="veggie" 로 넣었는데 경로 수정 필요한지 확인해주세요.
-// [ ] 추후 리팩토링이 필요합니다.
+export function Path({ title }: { title: string }) {
+  const pathname = usePathname();
 
-export function PathCaseOne({ title }: { title: string }) {
   return (
     <div className="flex text-3 gap-2">
       <Link href={`/`} className="text-oguogu-gray-4">
         홈
       </Link>
       <Image src={'/svgs/arrow-right.svg'} alt="arrowIcon" width={6} height={10} className="mb-[1px]" />
-      {document.referrer.includes('/search') ? (
+      {pathname.includes('/search') ? (
         <>
           <Link href="/search" className="text-oguogu-gray-4">
             검색
           </Link>
           <Image src="/svgs/arrow-right.svg" alt="arrowIcon" width={6} height={10} className="mb-[1px]" />
         </>
-      ) : document.referrer.includes('/product') ? (
+      ) : pathname.includes('/product') ? (
         <>
           <Link href="/product/crop" className="text-oguogu-gray-4">
             탐색
@@ -29,35 +30,35 @@ export function PathCaseOne({ title }: { title: string }) {
       ) : (
         ''
       )}
-
-      <Link href={`/`}>{title}</Link>
-    </div>
-  );
-}
-export function PathCaseTwo({ title, params, subParams }: { title: string; params: string; subParams: string }) {
-  return (
-    <div className="flex text-[12px] gap-[8px]">
-      <Link href={`/`} className="text-oguogu-gray-4">
-        홈
-      </Link>
-      <Image src={`/svgs/arrow-right.svg`} alt="arrowIcon" width={6} height={10} />
-      <Link href={`/product`} className="text-oguogu-gray-4">
-        탐색
-      </Link>
-      <Image src={'/svgs/arrow-right.svg'} alt="arrowIcon" width={6} height={10} />
-      <Link href={`/product/${params}/${subParams}`}>{title}</Link>
+      <Link href={`/search/result?keyword=${title}`}>{title}</Link>
     </div>
   );
 }
 
-export function PathCaseThree({ title, params, subParams }: { title: string; params: string; subParams: string }) {
-  return (
-    <div className="flex text-[12px] gap-[8px]">
-      <Link href={`/`} className="text-oguogu-gray-4">
-        홈
-      </Link>
-      <Image src={'/svgs/arrow-right.svg'} alt="arrowIcon" width={6} height={10} />
-      <Link href={`/product/${params}/${subParams}`}>{title}</Link>
-    </div>
-  );
-}
+// export function PathCaseTwo({ title, params, subParams }: { title: string; params: string; subParams: string }) {
+//   return (
+//     <div className="flex text-[12px] gap-[8px]">
+//       <Link href={`/`} className="text-oguogu-gray-4">
+//         홈
+//       </Link>
+//       <Image src={`/svgs/arrow-right.svg`} alt="arrowIcon" width={6} height={10} />
+//       <Link href={`/product`} className="text-oguogu-gray-4">
+//         탐색
+//       </Link>
+//       <Image src={'/svgs/arrow-right.svg'} alt="arrowIcon" width={6} height={10} />
+//       <Link href={`/product/${params}/${subParams}`}>{title}</Link>
+//     </div>
+//   );
+// }
+
+// export function PathCaseThree({ title, params, subParams }: { title: string; params: string; subParams: string }) {
+//   return (
+//     <div className="flex text-[12px] gap-[8px]">
+//       <Link href={`/`} className="text-oguogu-gray-4">
+//         홈
+//       </Link>
+//       <Image src={'/svgs/arrow-right.svg'} alt="arrowIcon" width={6} height={10} />
+//       <Link href={`/product/${params}/${subParams}`}>{title}</Link>
+//     </div>
+//   );
+// }
