5cfef6707c503acb73e02b080a630198b9ec7fbe
최영준 <jjmullan24@gmail.com>
2025-07-21 11:27:07 +0900

    💬 comment: 메인 페이지 항목 구분을 위한 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,14 +29,24 @@ export default function Home() {
 
   return (
     <>
+      {/* 헤더 */}
       <Header cartItemCount={100} />
+
+      {/* 네비게이션 */}
       <Navigation />
+
+      {/* 메인 슬라이드배너 */}
       <SlideBanner />
+
+      {/* 보조 네비게이션(aside) */}
       <aside className="flex flex-col gap-y-4 pt-6 mobile-max:gap-y-6 mobile-max:pt-8 ">
         <ImageCategory />
         <ProductLinkItem link="/product/crop" linkTitle="전체 농산물" subTxt="보러 가기" />
       </aside>
+
+      {/* 메인 */}
       <main className="flex flex-col gap-y-8 pt-8 mobile-max:gap-y-12 mobile-max:pt-12 ">
+        {/* 추천 카테고리 1 */}
         <article className="titleWithProductItem">
           <div className="flex flex-col gap-y-8">
             <Title title="온 가족이 함께 즐기는 텃밭 체험" description="7월 한 달 간 체험 상품 20% 할인!" />
@@ -50,6 +60,8 @@ export default function Home() {
             subTxt="보러 가기"
           />
         </article>
+
+        {/* 추천 카테고리 2 */}
         <article className="titleWithProductItem">
           <div className="flex flex-col gap-y-8">
             <Title title="톡! 터지는 옥수수의 반전 매력" description="7월 제철 채소 옥수수🌽" />
@@ -58,14 +70,17 @@ export default function Home() {
           <ProductLinkItem link="/search/result" linkTitle="옥수수" subTxt="구매하러 가기" />
         </article>
 
+        {/* 추천 카테고리 3 */}
         <article className="titleWithProductItem">
           <div className="flex flex-col gap-y-8">
             <Title title="마감 임박! 텃밭 상품 BEST 10" description="같이 심고, 같이 키우고, 같이 수확해요!" />
             <ProductItemList type="gardening" />
           </div>
-          <ProductLinkItem link="/search/result" linkTitle="텃밭 상품" subTxt="구경하러 가기" />
+          <ProductLinkItem link="/product/gardening" linkTitle="텃밭 상품" subTxt="구경하러 가기" />
         </article>
       </main>
+
+      {/* 푸터 */}
       <footer>
         <Link href="/search/result/1/detail">상세페이지로 이동하기 (임시)</Link>
         <button className="w-full bg-amber-100" type="button" onClick={login}>
