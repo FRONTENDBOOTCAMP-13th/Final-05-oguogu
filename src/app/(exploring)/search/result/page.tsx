eb900df9380d27208413af9544ceb1fbfe819217
최영준 <jjmullan24@gmail.com>
2025-07-21 13:21:32 +0900

    💄 style: 아이템 목록 정리를 위한 기본 Grid 스타일 클래스로 정의 및 수정 - grid-cols-[...] 값만 별도 클래스로 재정의하도록 구현

--- src/app/(exploring)/search/result/page.tsx
+++ src/app/(exploring)/search/result/page.tsx
@@ -14,7 +14,7 @@ export default function SearchResult() {
       <ProductSortbar />
 
       {/* 농산물(crop) 상품 */}
-      <main className="grid grid-cols-[repeat(auto-fit,minmax(140px,1fr))] gap-x-2 gap-y-8 px-4">
+      <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(140px,1fr))]">
         <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" originPrice={11800} />
         <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" originPrice={11800} />
         <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" originPrice={11800} />
@@ -24,14 +24,14 @@ export default function SearchResult() {
       </main>
 
       {/* 체험(experience) 상품 */}
-      <main className="grid grid-cols-[repeat(auto-fit,minmax(288px,1fr))] gap-x-2 gap-y-8 px-4">
+      <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(288px,1fr))]">
         <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" originPrice={10000} />
         <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" originPrice={10000} />
         <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" originPrice={10000} />
       </main>
 
       {/* 텃밭(gardening) 상품 */}
-      <main className="grid grid-cols-[repeat(auto-fit,minmax(140px,1fr))] gap-x-2 gap-y-8 px-4">
+      <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(140px,1fr))]">
         <GardenItem _id={1} name="초당옥수수 7월 수확" originPrice={10000} />
         <GardenItem _id={1} name="초당옥수수 7월 수확" originPrice={10000} />
         <GardenItem _id={1} name="초당옥수수 7월 수확" originPrice={10000} />
