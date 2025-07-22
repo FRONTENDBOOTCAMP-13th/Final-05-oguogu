fc662066c459a374cdd849ea476c5ff4b00ae857
최영준 <jjmullan24@gmail.com>
2025-07-22 22:59:37 +0900

    ✨ feat: HamburgerIcon 로그인 상태에 따른 경로 수정 로직 구현 - Zustand useAuthStore 를 통해 로그인 상태 검증

--- src/components/elements/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/elements/HamburgerIcon/HamburgerIcon.tsx
@@ -1,14 +1,18 @@
+import { useAuthStore } from '@/shared/store/authStore';
 import Link from 'next/link';
 
 export default function HamburgerIcon() {
-  // 로컬 스토리지에 접근해 accessToken 이 있는지 검증
+  // useAuthStore 의 로컬 스토리지 상태 검증 코드 사용
+  const isLoggedIn = useAuthStore(state => state.isLoggedIn);
+  console.log('\t로그인 상태: ', isLoggedIn);
 
-  // 있다면, 페이지 라우팅 경로 재설정
   return (
-    <Link href="/login" className="mr-8">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 12H20M4 18H20M4 6H20" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-      </svg>
-    </Link>
+    <>
+      <Link href={isLoggedIn ? '/mypage' : '/login'} className="mr-8">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 12H20M4 18H20M4 6H20" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </Link>
+    </>
   );
 }
