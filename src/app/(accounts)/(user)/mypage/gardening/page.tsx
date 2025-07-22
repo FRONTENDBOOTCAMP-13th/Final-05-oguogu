bba161bb1061db3f3040d8b19a4ba7fa93b1ee31
최영준 <jjmullan24@gmail.com>
2025-07-23 01:57:59 +0900

    🩹 fix: 세로 스크롤되는 현상 수정

--- src/app/(accounts)/(user)/mypage/gardening/page.tsx
+++ src/app/(accounts)/(user)/mypage/gardening/page.tsx
@@ -1,7 +1,7 @@
 export default function UserGardeningList() {
   return (
     <>
-      <main className="px-4">{/* 여기에 내용 작성 */}</main>
+      <main className="px-4 py-4 min-h-[calc(100vh-48px)]">{/* 여기에 내용 작성 */}</main>
     </>
   );
 }
--- src/app/(accounts)/(user)/mypage/page.tsx
+++ src/app/(accounts)/(user)/mypage/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function UserMyPage() {
   return (
     <>
-      <main className="px-4 pt-12 flex flex-col gap-4 text-2xl h-screen">
+      <main className="px-4 pt-12 flex flex-col gap-4 text-2xl min-h-[calc(100vh-48px)]">
         {/* 여기에 내용 작성 */}
         <Link href="/mypage/order">주문/배송 내역</Link>
         <Link href="/mypage/cart">장바구니</Link>
