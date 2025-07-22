bd764c4b4ae879e7c752529d0868fa84284c75c5
최영준 <jjmullan24@gmail.com>
2025-07-23 03:17:11 +0900

    ✨ feat: 인기 텃밭 순위 TOP 10 DB 반영 작업 및 스타일링 완료 - DB 중 데이터 추출에 필요한 Props 가 누락된 상태로, 현재는 정확하게 구현되지 않음 - 판매자 DB 를 넣고 테스트할 필요가 있음

--- src/components/elements/TopRatedItem/TopRatedItem.tsx
+++ src/components/elements/TopRatedItem/TopRatedItem.tsx
@@ -1,5 +1,6 @@
 import { getUsers } from '@/shared/data/functions/user';
 import { UserSeller } from '@/shared/types/user';
+import Link from 'next/link';
 
 /**
  * 인기 텃밭 TOP 10 컴포넌트
@@ -13,26 +14,51 @@ export default async function TopRatedItem() {
     .sort((a: UserSeller, b: UserSeller) => b.postViews - a.postViews)
     .slice(0, 10);
 
+  const sellerListTop5 = sellerList.slice(0, 5);
+  const sellerListRest5 = sellerList.slice(5, 10);
+
   return (
     <div className="w-full p-4 min-h-[calc(100vh-48px)]">
       <p className="mb-4 text-lg font-medium">인기 텃밭 순위 TOP10</p>
-      <ul className="grid grid-cols-2 gap-2" role="list" aria-label="인기 텃밭 순위">
-        {/* 데이터 중 item.extra.businessName 데이터 소실된 경우는 미포함 */}
-        {sellerList.map((item: UserSeller, index: number) => {
-          // if (item.extra.businessName) {
-          return (
-            <li
-              key={item._id}
-              className="flex items-center gap-2 p-3 transition-all shadow-sm cursor-pointer bg-oguogu-white rounded-xl hover:bg-gray-50 hover:shadow-md"
-            >
-              <span className="ml-1.5 font-medium text-md text-oguogu-main">
-                {index === 0 ? '🥇' : index === 1 ? '🥈' : index === 2 ? '🥉' : index + 1}
-              </span>
-              <span className="flex items-center h-6">{item.extra.businessName}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="grid grid-cols-2 gap-2" role="list" aria-label="인기 텃밭 순위">
+        {/* Top 1~5 */}
+        <ul className="flex flex-col gap-2">
+          {sellerListTop5.map((item: UserSeller, index: number) => {
+            return (
+              <li
+                key={item._id}
+                className="flex items-center gap-2 p-3 transition-all shadow-sm cursor-pointer bg-oguogu-white rounded-xl hover:bg-gray-50 hover:shadow-md"
+              >
+                <span className="ml-1.5 font-medium text-md text-oguogu-main">
+                  {index === 0 ? '🥇' : index === 1 ? '🥈' : index === 2 ? '🥉' : index + 1}
+                </span>
+                {/* 해당 아이템의 _id 와 매칭하는 판매자 채널로 경로 설정 */}
+                <Link href={`/gardening/${item._id}`} className="flex items-center h-6">
+                  {item.extra.businessName}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* Top 6~10 */}
+        <ul className="flex flex-col gap-2">
+          {sellerListRest5.map((item: UserSeller, index: number) => {
+            // if (item.extra.businessName) {
+            return (
+              <li
+                key={item._id}
+                className="flex items-center gap-2 p-3 transition-all shadow-sm cursor-pointer bg-oguogu-white rounded-xl hover:bg-gray-50 hover:shadow-md"
+              >
+                <span className="ml-1.5 font-medium text-md text-oguogu-main">{index + 6}</span>
+                <Link href={`/gardening/${item._id}`} className="flex items-center h-6">
+                  {item.extra.businessName}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
