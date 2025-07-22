6cb71ee685ff6ec33e159fe849f8be3ca0bff9ae
최영준 <jjmullan24@gmail.com>
2025-07-22 15:54:33 +0900

    🩹 fix: mypage 하위 더미 링크 삽입

--- src/app/(accounts)/(user)/mypage/page.tsx
+++ src/app/(accounts)/(user)/mypage/page.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 export default function UserMyPage() {
   return (
     <>
-      <main className="px-4">{/* 여기에 내용 작성 */}dd</main>
+      <main className="px-4">
+        {/* 여기에 내용 작성 */}
+        <Link href="/mypage/order">주문/배송 내역</Link>
+        <Link href="/mypage/cart">장바구니</Link>
+        <Link href="/mypage/pick">찜한 상품</Link>
+        <Link href="/mypage/gardening">텃밭 구독</Link>
+      </main>
     </>
   );
 }
