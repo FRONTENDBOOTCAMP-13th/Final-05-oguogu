e4a59cbdfaa94b3c38c7e4e45984a6eb8b380be6
jaehyun0620 <zzz6451@naver.com>
2025-07-23 11:13:17 +0900

    ✨ feat: 주문 상태 컴포넌트 UI 구현

--- src/app/(accounts)/(user)/mypage/order/page.tsx
+++ src/app/(accounts)/(user)/mypage/order/page.tsx
@@ -1,10 +1,14 @@
+import OrderItem from '@/components/elements/OrderItem/OrderItem';
 import LinkHeader from '@/components/layouts/Header/LinkHeader';
 
 export default function UserOrderList() {
   return (
     <>
       <LinkHeader title="주문정보" />
-      <main className="px-4">{/* 여기에 내용 작성 */}</main>
+      <main className="px-4">
+        {/* 여기에 내용 작성 */}
+        <OrderItem orderState="delivered" />
+      </main>
     </>
   );
 }
