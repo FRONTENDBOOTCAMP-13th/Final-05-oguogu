dc97e2072ca9c0199f0a3cd71acd0d5d926145b1
최영준 <jjmullan24@gmail.com>
2025-07-23 15:03:53 +0900

    🩹 fix: SearchHeader 기능 개선 - Zustand 상태 저장 로직을 활용 - 키워드가 URL 에 쿼리스트링 형태로 작성

--- src/app/(exploring)/search/page.tsx
+++ src/app/(exploring)/search/page.tsx
@@ -1,4 +1,3 @@
-import CropItem from '@/components/elements/ProductItem/Item/CropItem';
 import TopRatedItem from '@/components/elements/TopRatedItem/TopRatedItem';
 import SearchHeader from '@/components/layouts/Header/SearchHeader';
 
@@ -6,18 +5,13 @@ export default function Search() {
   return (
     <>
       <SearchHeader />
+
       {/* 검색 버튼을 클릭했을 때, 해당 컴포넌트가 안보이고 상품 배열이 나타나는 형태로 구현 */}
+      {/* 인기 텃밭 Top10 : URL 에 쿼리스트링이 있으면 숨김 */}
       <TopRatedItem />
 
-      {/* 농산물(crop) 상품 */}
-      <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(140px,1fr))]">
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-        <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-      </main>
+      {/* 상품 목록 : URL 에 ?keyword={finKeyword} 형태로 키워드가 있을 때 해당 데이터와 일치하는 데이터를 추출 */}
+      <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(140px,1fr))]"></main>
     </>
   );
 }
