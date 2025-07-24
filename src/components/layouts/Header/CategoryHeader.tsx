750b9db5aa1b9059882eb54d3a04723d9786cc98
ji-yeoni <wldua16354@naver.com>
2025-07-24 18:30:32 +0900

    🐛 bugfix: 버그 수정

--- src/components/layouts/Header/CategoryHeader.tsx
+++ src/components/layouts/Header/CategoryHeader.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { HeaderExtraProps } from '@/components/layouts/Header/types/Header.type';
 import GoBackIcon from '@/features/goBackIcon/goBackIcon';
 import CartItemCountIcon from '@/features/cartItemCountIcon/CartItemCountIcon';
 
-export default function CategoryHeader({ title }: HeaderExtraProps) {
+export default function CategoryHeader({ title }: { title: string }) {
   return (
     <header className="header">
       {/* 검색 + 버튼 */}
@@ -12,7 +11,7 @@ export default function CategoryHeader({ title }: HeaderExtraProps) {
         <GoBackIcon />
 
         {/* INFO 쿼리스트링 존재 여부 검증 및 현재 위치(pathname) 기반 텍스트 동적 변경 필요 */}
-        <h1 className="textElipsis flex-1 h-6 sm:w-48 ml-2 pl-2 text-lg">{title}</h1>
+        <h1 className="flex-1 h-6 pl-2 ml-2 text-lg textElipsis sm:w-48">{title}</h1>
 
         {/* 장바구니 아이콘 + 뱃지 */}
         <CartItemCountIcon />
