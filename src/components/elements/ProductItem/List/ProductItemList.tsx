92427da9578375173fb58615786e89f487f2888a
최영준 <jjmullan24@gmail.com>
2025-07-24 16:31:28 +0900

    ✨ feat: 메인 페이지 컴포넌트 추천 아이템 제공 기능 구현 완료

--- src/components/elements/ProductItem/List/ProductItemList.tsx
+++ src/components/elements/ProductItem/List/ProductItemList.tsx
@@ -7,17 +7,28 @@ import { Item } from '@/shared/types/product';
 
 export default async function ProductItemList({ type }: ProductItemListType) {
   const res = await getProducts();
-  const filteredData = res.item
+
+  const cropData = res.item
     .filter((item: Item) => item.name.includes('옥수수'))
     .filter((item: Item) => item.extra?.productType === 'crop')
     .sort((a: Item, b: Item) => a.extra!.likeCount < b.extra!.likeCount);
-  console.log(filteredData);
+  console.log(cropData);
+
+  const expData = res.item
+    .filter((item: Item) => item.extra?.productType === 'experience')
+    .sort((a: Item, b: Item) => a.extra!.likeCount < b.extra!.likeCount);
+  console.log(expData);
+
+  const gardeningData = res.item
+    .filter((item: Item) => item.extra?.productType === 'gardening')
+    .sort((a: Item, b: Item) => a.extra!.likeCount < b.extra!.likeCount);
+  console.log(gardeningData);
 
   return (
     <>
       {type === 'crop' ? (
         <div className="flex gap-3 overflow-x-scroll cursor-grab select-none no-scrollbar">
-          {filteredData
+          {cropData
             .map((item: Item) => (
               <CropItem
                 key={item._id}
@@ -35,24 +46,37 @@ export default async function ProductItemList({ type }: ProductItemListType) {
         </div>
       ) : type === 'experience' ? (
         <div className="flex gap-3 overflow-x-scroll cursor-grab select-none no-scrollbar">
-          <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" price={10000} />
-          <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" price={10000} />
-          <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" price={10000} />
-          <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" price={10000} />
-          <ExperienceItem _id={1} name="[7/25] 감자캐기 체험" price={10000} />
+          {expData.map((item: Item) => (
+            <ExperienceItem
+              key={item._id}
+              _id={item._id}
+              name={item.name}
+              price={item.price}
+              rating={item.rating}
+              bookmarks={item.bookmarks}
+              extra={item.extra}
+              seller={item.seller}
+              replies={item.replies}
+            />
+          ))}
         </div>
       ) : type === 'gardening' ? (
         <div className="flex gap-3 overflow-x-scroll cursor-grab select-none no-scrollbar">
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
-          <GardenItem _id={1} name="초당옥수수 7월 수확" price={10000} />
+          {gardeningData.map((item: Item) => (
+            <GardenItem
+              key={item._id}
+              _id={item._id}
+              name={item.name}
+              price={item.price}
+              rating={item.rating}
+              bookmarks={item.bookmarks}
+              extra={item.extra}
+              seller={item.seller}
+              replies={item.replies}
+              quantity={item.quantity}
+              buyQuantity={item.buyQuantity}
+            />
+          ))}
         </div>
       ) : (
         ''
