9ad0c45545d156aec1c75ad8bc27728796cb3500
최영준 <jjmullan24@gmail.com>
2025-07-24 15:23:15 +0900

    🩹 fix: LinkHeader 로직 변경으로 인한 각 페이지 레이아웃 컴포넌트 추가

--- src/app/(accounts)/(user)/mypage/layout.tsx
+++ src/app/(accounts)/(user)/mypage/layout.tsx
@@ -1,11 +1,5 @@
-import LinkHeader from '@/components/layouts/Header/LinkHeader';
 import React from 'react';
 
 export default function MypageRootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <LinkHeader />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
