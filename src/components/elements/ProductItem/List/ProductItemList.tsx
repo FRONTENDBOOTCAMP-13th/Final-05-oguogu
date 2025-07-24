44771b72b886ca286a62feb0c231fb0d3bcb2b3d
최영준 <jjmullan24@gmail.com>
2025-07-24 16:11:52 +0900

    ✨ feat: 메인 페이지 내 필터링된 데이터를 DB에 반영하는 작업 1차 완료

--- src/components/elements/ProductItem/List/ProductItemList.tsx
+++ src/components/elements/ProductItem/List/ProductItemList.tsx
@@ -2,22 +2,36 @@ import CropItem from '@/components/elements/ProductItem/Item/CropItem';
 import ExperienceItem from '@/components/elements/ProductItem/Item/ExperienceItem';
 import GardenItem from '@/components/elements/ProductItem/Item/GardenItem';
 import { ProductItemListType } from '@/components/elements/ProductItem/List/ProductItem.type';
+import { getProducts } from '@/shared/data/functions/product';
+import { Item } from '@/shared/types/product';
 
-export default function ProductItemList({ type }: ProductItemListType) {
+export default async function ProductItemList({ type }: ProductItemListType) {
+  const res = await getProducts();
+  const filteredData = res.item
+    .filter((item: Item) => item.name.includes('옥수수'))
+    .filter((item: Item) => item.extra?.productType === 'crop')
+    .sort((a: Item, b: Item) => a.extra!.likeCount < b.extra!.likeCount);
+  console.log(filteredData);
+
   return (
     <>
       {type === 'crop' ? (
         <div className="flex gap-3 overflow-x-scroll cursor-grab select-none no-scrollbar">
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
-          <CropItem _id={1} name="쫀득쫀득 대학 미백 찰옥수수 30개입" price={11800} />
+          {filteredData
+            .map((item: Item) => (
+              <CropItem
+                key={item._id}
+                _id={item._id}
+                name={item.name}
+                price={item.price}
+                rating={item.rating}
+                bookmarks={item.bookmarks}
+                extra={item.extra}
+                seller={item.seller}
+                replies={item.replies}
+              />
+            ))
+            .slice(0, 10)}
         </div>
       ) : type === 'experience' ? (
         <div className="flex gap-3 overflow-x-scroll cursor-grab select-none no-scrollbar">
