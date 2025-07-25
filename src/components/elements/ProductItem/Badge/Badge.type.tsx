10fc584f706ea5ab7b4104aab38d0118b0c1556f
최영준 <jjmullan24@gmail.com>
2025-07-25 14:38:43 +0900

    ✨ feat: Badge 종류 텍스트 타입 추가

--- src/components/elements/ProductItem/Badge/Badge.type.tsx
+++ src/components/elements/ProductItem/Badge/Badge.type.tsx
@@ -1,3 +1,5 @@
+export type BadgeTextProps = 'sold' | 'best' | 'inseason' | 'new' | 'lowstock' | 'express' | 'safe';
+
 export default interface BadgeProps {
   type: 'inseason' | 'best' | 'lowstock' | 'sold' | 'new' | 'express' | 'safe';
   size?: number;
