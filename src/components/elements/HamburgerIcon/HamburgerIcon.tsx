62e4a308276e8e3b58b3a689f2f48e9d5bd2a93c
최영준 <jjmullan24@gmail.com>
2025-07-27 22:34:54 +0900

    ✨ feat: 햄버거 버튼에 '로그인' 표시 기능 추가

--- src/components/elements/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/elements/HamburgerIcon/HamburgerIcon.tsx
@@ -7,10 +7,13 @@ import Link from 'next/link';
 export default function HamburgerIcon() {
   return (
     <>
-      <Link href="/mypage" className="mr-8">
+      <Link href="/mypage" className="mr-8 relative">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 12H20M4 18H20M4 6H20" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
+        <span className="absolute top-[-5px] right-[-30px] text-[8px] bg-[#fff000] px-1.5 py-0.5 rounded-4xl">
+          로그인
+        </span>
       </Link>
     </>
   );
