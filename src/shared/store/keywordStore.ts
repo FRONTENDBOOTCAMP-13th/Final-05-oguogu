b379dd51a5b4f5c8e6af614041c10ad8d99eb01f
최영준 <jjmullan24@gmail.com>
2025-07-27 23:11:28 +0900

    🩹 fix: useSearchKeywordStore Get 메서드 추가 - originKeyword 값을 가져오기 위한 Get 메서드 추가

--- src/shared/store/keywordStore.ts
+++ src/shared/store/keywordStore.ts
@@ -4,10 +4,11 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 export const useSearchKeywordStore = create<keywordState>()(
   persist(
-    set => ({
+    (set, get) => ({
       originKeyword: null,
       saveKeyword: (keyword: string) => set({ originKeyword: keyword }),
       clearKeyword: () => set({ originKeyword: null }),
+      getKeyword: () => get().originKeyword,
     }),
     {
       name: 'keyword',
