562fbf19c62f8a830f17995e7017695defdd57c5
최영준 <jjmullan24@gmail.com>
2025-07-27 23:35:24 +0900

    💬 comment: 불필요한 주석 삭제

--- src/app/(exploring)/search/page.tsx
+++ src/app/(exploring)/search/page.tsx
@@ -6,7 +6,6 @@ export default function Search() {
     <>
       <SearchHeader />
 
-      {/* 검색 버튼을 클릭했을 때, 해당 컴포넌트가 안보이고 상품 배열이 나타나는 형태로 구현 */}
       {/* 인기 텃밭 Top10 : URL 에 쿼리스트링이 있으면 숨김 */}
       <TopRatedItem />
     </>
