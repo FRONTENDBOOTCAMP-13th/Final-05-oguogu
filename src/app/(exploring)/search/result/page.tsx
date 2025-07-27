6ff7bc96d9ee7c7a47f4784953a89eccdd3740a5
최영준 <jjmullan24@gmail.com>
2025-07-27 23:52:46 +0900

    🩹 fix: 검색 결과 페이지 컴포넌트 분리

--- src/app/(exploring)/search/result/page.tsx
+++ src/app/(exploring)/search/result/page.tsx
@@ -1,13 +1,7 @@
-'use client';
-
 import SearchHeader from '@/components/layouts/Header/SearchHeader';
 import ProductListFilteredKeyword from '@/features/productListFilteredKeyword/productListFilteredKeyword';
-import { useSearchKeywordStore } from '@/shared/store/keywordStore';
 
 export default function SearchResult() {
-  /* Zustand 에서 저장한 키워드 값을 가져오는 코드 */
-  const getKeyword = useSearchKeywordStore(state => state.originKeyword);
-
   return (
     <>
       {/* 헤더 */}
@@ -16,9 +10,9 @@ export default function SearchResult() {
       {/* 정렬바 */}
       {/* <ProductSortbar cnt={filtered.length} /> */}
 
-      {/* 상품 목록 : Zustand 에 저장되어 있는 키워드를 params 로 전달 */}
+      {/* 상품 목록 */}
       <main className="itemGrid grid-cols-[repeat(auto-fit,minmax(140px,1fr))]">
-        <ProductListFilteredKeyword keyword={getKeyword!} />
+        <ProductListFilteredKeyword />
       </main>
     </>
   );
