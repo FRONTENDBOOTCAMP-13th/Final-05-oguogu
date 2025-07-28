d5b102fbe63a85bd8757490230a63b7513678e74
최영준 <jjmullan24@gmail.com>
2025-07-28 17:42:36 +0900

    ✨ feat: 햄버거 버튼 조건 수정 - 스토리지의 userInfo.type 의 값에 따라 라우팅 경로 변경 로직

--- src/components/elements/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/elements/HamburgerIcon/HamburgerIcon.tsx
@@ -10,7 +10,18 @@ export default function HamburgerIcon() {
 
   return (
     <>
-      <Link href="/mypage" className="mr-8 relative">
+      <Link
+        href={
+          loginData.userInfo?.type === 'user'
+            ? '/mypage'
+            : loginData.userInfo?.type === 'seller'
+              ? '/office'
+              : loginData.userInfo?.type === 'admin'
+                ? '/admin'
+                : '/mypage'
+        }
+        className="mr-8 relative"
+      >
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 12H20M4 18H20M4 6H20" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
