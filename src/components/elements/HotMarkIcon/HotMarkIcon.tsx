d6658161c53340906c7bf93d16582e115da65f68
최영준 <jjmullan24@gmail.com>
2025-07-28 17:44:33 +0900

    💄 style: '인기(HotMarkIcon)' 컴포넌트 애니메이션 추가

--- src/components/elements/HotMarkIcon/HotMarkIcon.tsx
+++ src/components/elements/HotMarkIcon/HotMarkIcon.tsx
@@ -1,5 +1,7 @@
 export default function HotMarkIcon() {
   return (
-    <span className="absolute t-0 r-0 translate-y-[-4px] text-[8px] bg-[#fff000] px-1.5 py-0.5 rounded-4xl">인기</span>
+    <span className="absolute t-0 r-0 translate-y-[-4px] text-[8px] bg-[#fff000] px-1.5 py-0.5 rounded-4xl animate-[floatUpDown_1.5s_ease-in-out_infinite]">
+      인기
+    </span>
   );
 }
