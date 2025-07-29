9ca9eb3d04433f674f5c6204ba1040f9fed0c21e
jwe0516 <jwe0516@naver.com>
2025-07-29 17:20:23 +0900

    💄style : 판매자 관리페이지 하위 정산 페이지 헤더위치 수정

--- src/app/(accounts)/(seller)/office/products/select/page.tsx
+++ src/app/(accounts)/(seller)/office/products/select/page.tsx
@@ -3,15 +3,17 @@ import LinkHeader from '@/components/layouts/Header/LinkHeader';
 
 export default function SelectProductType() {
   return (
-    <main className="px-4 py-4 flex flex-col gap-2 min-h-[calc(100vh-48px)]">
+    <>
       <LinkHeader title="상품 등록" />
-      <div className="flex flex-col gap-4 mt-4">
-        <SelectProductItem params="crop" title="농산물" />
+      <main className="px-4 py-4 flex flex-col gap-2 min-h-[calc(100vh-48px)]">
+        <div className="flex flex-col gap-4 mt-4">
+          <SelectProductItem params="crop" title="농산물" />
 
-        <SelectProductItem params="experience" title="체험" />
+          <SelectProductItem params="experience" title="체험" />
 
-        <SelectProductItem params="gardening" title="텃밭" />
-      </div>
-    </main>
+          <SelectProductItem params="gardening" title="텃밭" />
+        </div>
+      </main>
+    </>
   );
 }
