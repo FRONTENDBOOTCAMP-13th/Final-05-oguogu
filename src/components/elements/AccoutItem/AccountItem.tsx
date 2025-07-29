76cc653c7a35f295d48a91d1f7c15c24f16b6a6f
ji-yeoni <wldua16354@naver.com>
2025-07-29 16:06:33 +0900

    ✨ feat: 판매자 관리페이지 하위 정산 페이지 레이아웃 - 계좌 폼 컴포넌트 분리 제작

--- src/components/elements/AccoutItem/AccountItem.tsx
+++ src/components/elements/AccoutItem/AccountItem.tsx
@@ -1,85 +1,34 @@
-'use client';
+import AccountForm from '@/components/elements/AccoutItem/AccountForm';
 
-import LoginInput from '@/components/elements/LoginItem/LoginInput';
-import { useState } from 'react';
-
-export default function AccountForm() {
-  const [bank, setBank] = useState('');
-  const [owner, setOwner] = useState('');
-  const [accountNumber, setAccountNumber] = useState('');
-
+export default function AccountItem() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="max-w-[320px] w-full p-4 space-y-4 bg-white border border-oguogu-gray-2 rounded-lg drop-shadow">
-        {/* 은행 선택 */}
-        <div className="flex flex-col">
-          <label htmlFor="bank">
-            은행<sup className="text-[10px]">*</sup>
-          </label>
-          <select
-            id="bank"
-            value={bank}
-            onChange={e => setBank(e.target.value)}
-            className="text-xs text-center border rounded h-7 border-oguogu-gray-2 text-oguogu-gray-4"
-          >
-            <option value="">은행 선택</option>
-            <option value="국민은행">국민은행</option>
-            <option value="신한은행">신한은행</option>
-            <option value="우리은행">우리은행</option>
-            <option value="하나은행">하나은행</option>
-            <option value="카카오뱅크">카카오뱅크</option>
-            <option value="농협">농협</option>
-            <option value="기업은행">기업은행</option>
-          </select>
-        </div>
-
-        {/* 계좌주 */}
-        <div className="flex flex-col">
-          <label htmlFor="owner">
-            계좌주<sup className="text-[10px]">*</sup>
-          </label>
-          <LoginInput
-            id="owner"
-            type="text"
-            placeholder="실제 계좌를 소유한 사람의 이름을 입력해 주세요"
-            value={owner}
-            onChange={setOwner}
-          />
-        </div>
-
-        {/* 계좌번호 */}
-        <div className="flex flex-col">
-          <label htmlFor="accountNumber">
-            계좌번호<sup className="text-[10px]">*</sup>
-          </label>
-          <LoginInput
-            id="accountNumber"
-            type="text"
-            value={accountNumber}
-            onChange={setAccountNumber}
-            placeholder="-를 제외한 숫자만 입력해 주세요"
-          />
-        </div>
-
-        <section className="flex justify-center items-center gap-2 text-[12px]">
-          <button
-            onClick={() => {
-              console.log({ bank, owner, accountNumber });
-              if (!bank || !owner || !accountNumber) {
-                alert('모든 항목을 입력해 주세요.');
-                return;
-              }
-              alert('계좌가 등록되었습니다!');
-            }}
-            className="w-full mt-2 text-xs text-white rounded h-7 bg-oguogu-main hover:bg-oguogu-main-dark"
-          >
-            정산 계좌 등록
-          </button>
-          <button className="w-full mt-2 text-xs border rounded h-7 text-oguogu-black border-oguogu-gray-2 bg-oguogu-white hover:bg-oguogu-gray-1">
-            취소
-          </button>
+    <div className="flex flex-col justify-start min-h-screen gap-2 p-4">
+      <div className="p-4">
+        {/* 정산 금액 */}
+        <section className="flex flex-col items-center gap-2 p-4 pb-8 text-base">
+          <p>흥부네농산물님의 7월 정산 예정 금액은</p>
+          <p>
+            <span className="text-2xl font-bold text-oguogu-main">8,354,980</span>
+            <span className="text-2xl text-oguogu-black">원 입니다.</span>
+          </p>
+        </section>
+        {/* 정산 정보 */}
+        <section className="flex flex-col gap-2 pt-4 pb-4 text-xs border-t border-b border-oguogu-gray-2">
+          <div className="flex gap-1">
+            <span className="text-oguogu-gray-4 min-w-[80px]">정산 주기</span>
+            <span>매월 10일</span>
+          </div>
+          <div className="flex gap-1">
+            <span className="text-oguogu-gray-4 min-w-[80px]">정산 대상 기간</span>
+            <span>2025.07.01 ~ 2025.07.31</span>
+          </div>
+          <div className="flex gap-1">
+            <span className="text-oguogu-gray-4 min-w-[80px]">정산 계좌</span>
+            <span>미등록</span>
+          </div>
         </section>
       </div>
+      <AccountForm />
     </div>
   );
 }
