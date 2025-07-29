277a632cad46519c218e5c82212eacce22038829
최영준 <jjmullan24@gmail.com>
2025-07-29 11:37:30 +0900

    ✨ feat: 푸터 1차 완성

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="px-4 py-6 w-full text-xs flex flex-col gap-3 text-oguogu-gray-4 ">
-      <p className="text-base text-oguogu-black">고객센터</p>
-      <div className="flex flex-col">
+    <footer className="p-4 w-full text-xs flex flex-col gap-3 text-oguogu-gray-4 ">
+      <p className="pt-2 text-base text-oguogu-black">고객센터</p>
+      <div className="flex flex-col gap-y-0.5">
         <div className="flex gap-1">
           <p>운영시간 :</p>
           <div className="flex gap-1">
@@ -15,23 +15,25 @@ export default function Footer() {
         </div>
         <div className="flex gap-1">
           <p>대표전화 :</p>
-          <p>02-5959-5959</p>
+          <p>070-5959-5959</p>
         </div>
         <div className="flex gap-1">
           <p>문의 :</p>
-          <p>help@ogugarden.com</p>
+          <Link href="mailto:help@ogugarden.com" className="cursor-pointer hover:text-oguogu-black">
+            help@ogugarden.com
+          </Link>
         </div>
       </div>
 
-      <p className="text-base text-oguogu-black">회사정보</p>
-      <div className="flex flex-col">
+      <p className="pt-2 text-base text-oguogu-black">회사정보</p>
+      <div className="flex flex-col gap-y-0.5">
         <div className="flex gap-1">
           <p>상호명 :</p>
-          <p>오구텃밭</p>
+          <p>멋사 프론트엔드 13기 파이널 프로젝트 5조 &apos;오구텃밭&apos;</p>
         </div>
         <div className="flex gap-1">
           <p>대표자명 :</p>
-          <p>김오구</p>
+          <p>김재현, 김지연, 정원식, 최영준</p>
         </div>
         <div className="flex gap-1">
           <p>사업자 등록번호 :</p>
@@ -43,15 +45,15 @@ export default function Footer() {
         </div>
       </div>
 
-      <p className="text-base text-oguogu-black">이용약관 및 정책</p>
-      <div className="flex flex-col">
+      <p className="pt-2 text-base text-oguogu-black">이용약관 및 정책</p>
+      <div className="flex flex-col gap-y-0.5">
         <Link href="/">이용약관</Link>
         <Link href="/">개인정보처리방침</Link>
         <Link href="/">전자금융거래 이용약관</Link>
         <Link href="/">배송/교환/환불 정책</Link>
       </div>
 
-      <p className="text-base text-oguogu-black">SNS</p>
+      <p className="pt-2 text-base text-oguogu-black">SNS</p>
       <Link href="/" className="flex gap-1">
         <Image src="/svgs/instagram.svg" alt="instagram" width={24} height={24} />
         <Image src="/svgs/x-twitter.svg" alt="x(twitter)" width={24} height={24} />
