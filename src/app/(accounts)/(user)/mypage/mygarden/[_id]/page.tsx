81aafbcf0574e41c1d71405c970ef348e223aad7
최영준 <jjmullan24@gmail.com>
2025-07-31 07:54:29 +0900

    ✨ feat: 텃밭 히스토리 -> 개별 상품 페이지 레이아웃 초안

---
+++ src/app/(accounts)/(user)/mypage/mygarden/[_id]/page.tsx
@@ -0,0 +1,10 @@
+import LinkHeader from '@/components/layouts/Header/LinkHeader';
+
+export default function MyGardenItemPage() {
+  return (
+    <>
+      <LinkHeader title="텃밭 히스토리" bgColor="[#DBFCE7]" />
+      <main className="px-4 py-4 min-h-[calc(100vh-48px)]"></main>
+    </>
+  );
+}
