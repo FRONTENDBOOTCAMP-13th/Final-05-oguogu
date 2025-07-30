9c3a97c61836ee73d2d9a7011134d6a7452c487c
최영준 <jjmullan24@gmail.com>
2025-07-31 07:52:53 +0900

    ✨ feat: 텃밭 히스토리 페이지 기능 완성 - 텃밭 개별 페이지 UI 작업 및 기능 추가 필요

--- src/components/elements/GardenItemForMyGarden/GardenItemForMyGarden.tsx
+++ src/components/elements/GardenItemForMyGarden/GardenItemForMyGarden.tsx
@@ -1,11 +1,26 @@
+import { periodObject } from '@/shared/types/product';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function GardenItemForMyGarden() {
+export default function GardenItemForMyGarden({ name, period, id }: { name: string; period: []; id: number }) {
+  const allStatus = period.map((item: periodObject) => item.status);
+
+  /* period 의 status 값을 추출, 포함 여부를 검증하여 가장 마지막 데이터를 렌더링 */
+  let lastStatus = '';
+  allStatus.map((status: string) =>
+    status.includes('harvest')
+      ? (lastStatus = '수확 완료')
+      : status.includes('growing')
+        ? (lastStatus = '성장')
+        : status.includes('sprouting')
+          ? (lastStatus = '발아')
+          : (lastStatus = '파종'),
+  );
+
   return (
     <>
       <Link
-        href={`/mypage/mygarden/id`}
+        href={`/mypage/mygarden/${id}`}
         className="flex justify-center items-center bg-oguogu-main-light border-2 border-oguogu-main rounded-lg relative min-w-[85px] w-full min-h-[85px] h-full aspect-square"
       >
         <div className="flex flex-col items-center gap-y-1 translate-y-1.5">
@@ -13,8 +28,8 @@ export default function GardenItemForMyGarden() {
             <Image src={`/svgs/sowing.svg`} alt={`sowing`} width={56} height={56} className="" />
           </div>
           <div className="flex flex-col text-center">
-            <p className="text-xs mobile-max:text-base">브로콜리</p>
-            <p className="text-[10px] mobile-max:text-sm text-oguogu-main">파종</p>
+            <p className="text-xs mobile-max:text-base">{name}</p>
+            <p className="text-[10px] mobile-max:text-sm text-oguogu-main">{lastStatus}</p>
           </div>
         </div>
         <div className="w-4 h-4 border-2 bg-oguogu-yellow border-oguogu-main-light absolute rounded-3xl bottom-0 right-0 translate-1/2 mobile-max:w-6 mobile-max:h-6 mobile-max:border-3"></div>
