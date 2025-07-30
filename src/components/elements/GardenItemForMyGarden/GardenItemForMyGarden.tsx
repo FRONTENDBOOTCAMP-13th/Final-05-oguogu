3d8fe580f4bfcc4923c8064ec554cac0b3fbf9f8
최영준 <jjmullan24@gmail.com>
2025-07-31 08:19:01 +0900

    ✨ feat: 업로드된 날짜에 따라 텃밭 히스토리에서 상태를 볼 수 있는 기능 구현 - 가장 최근 업로드된 날짜가 오늘 ~ 5일 전일 경우, 노란색 표시 - 오늘 기준 6일 이전, 또는 DB 오류로 0 이상인 경우 회색 표시

--- src/components/elements/GardenItemForMyGarden/GardenItemForMyGarden.tsx
+++ src/components/elements/GardenItemForMyGarden/GardenItemForMyGarden.tsx
@@ -1,11 +1,20 @@
 import { periodObject } from '@/shared/types/product';
+import getDaysFromToday from '@/utils/getDaysFromToday/getDaysFromToday';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function GardenItemForMyGarden({ name, period, id }: { name: string; period: []; id: number }) {
+export default function GardenItemForMyGarden({
+  name,
+  period,
+  id,
+}: {
+  name: string;
+  period: periodObject[];
+  id: number;
+}) {
+  /* period 의 status 값을 추출, 포함 여부를 검증하여 가장 마지막 데이터를 렌더링 */
   const allStatus = period.map((item: periodObject) => item.status);
 
-  /* period 의 status 값을 추출, 포함 여부를 검증하여 가장 마지막 데이터를 렌더링 */
   let lastStatus = '';
   allStatus.map((status: string) =>
     status.includes('harvest')
@@ -17,13 +26,26 @@ export default function GardenItemForMyGarden({ name, period, id }: { name: stri
           : (lastStatus = '파종'),
   );
 
+  /* 판매자가 업로드한 최근 게시물의 날짜가 오늘 기준 3일 이내일 때 색으로 표시하는 기능 */
+  const lastDate = period.map((item: periodObject) => item.date);
+  const getDaysFromLastDate = getDaysFromToday(lastDate).pop() ?? 1;
+
+  let isRecent = false;
+  if (getDaysFromLastDate > 0) {
+    isRecent = false;
+  } else if (getDaysFromLastDate < 0 && getDaysFromLastDate > -5) {
+    isRecent = true;
+  } else {
+    isRecent = false;
+  }
+
   return (
     <>
       <Link
         href={`/mypage/mygarden/${id}`}
         className="flex justify-center items-center bg-oguogu-main-light border-2 border-oguogu-main rounded-lg relative min-w-[85px] w-full min-h-[85px] h-full aspect-square"
       >
-        <div className="flex flex-col items-center gap-y-1 translate-y-1.5">
+        <div className="flex flex-col items-center gap-y-1 translate-y-1">
           <div className="w-7 h-7 mobile-max:w-10 mobile-max:h-10 flex items-center justify-center">
             <Image src={`/svgs/sowing.svg`} alt={`sowing`} width={56} height={56} className="" />
           </div>
@@ -32,7 +54,9 @@ export default function GardenItemForMyGarden({ name, period, id }: { name: stri
             <p className="text-[10px] mobile-max:text-sm text-oguogu-main">{lastStatus}</p>
           </div>
         </div>
-        <div className="w-4 h-4 border-2 bg-oguogu-yellow border-oguogu-main-light absolute rounded-3xl bottom-0 right-0 translate-1/2 mobile-max:w-6 mobile-max:h-6 mobile-max:border-3"></div>
+        <div
+          className={`w-5 h-5 border-2 bg-oguogu-${isRecent ? 'main' : 'gray-2'} border-oguogu-main-light absolute rounded-3xl top-0 right-0 translate-y-[-10px] translate-x-1/2 mobile-max:border-3`}
+        ></div>
       </Link>
     </>
   );
