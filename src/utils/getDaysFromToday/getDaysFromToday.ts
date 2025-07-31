b5f60ad8debb6a9a062573c0dfb1888dae42b2e4
최영준 <jjmullan24@gmail.com>
2025-07-31 10:22:50 +0900

    🩹 fix: getDaysFromToday 오류 수정 (개선 필요)

--- src/utils/getDaysFromToday/getDaysFromToday.ts
+++ src/utils/getDaysFromToday/getDaysFromToday.ts
@@ -1,12 +1,7 @@
 import { parseISO, differenceInDays } from 'date-fns';
 
-export default function getDaysFromToday({ startDate, endDates }: { startDate?: string; endDates: string[] }) {
-  let today: Date | string = new Date(); // 오늘 날짜
-
-  // 만약 startDate 가 있으면
-  if (startDate) {
-    today = parseISO(startDate);
-  }
+export default function getDaysFromToday(endDates: string[]) {
+  const today = new Date(); // 오늘 날짜
 
   return endDates.map(endDates => {
     const parsedDate = parseISO(endDates); // 문자열 -> Date
