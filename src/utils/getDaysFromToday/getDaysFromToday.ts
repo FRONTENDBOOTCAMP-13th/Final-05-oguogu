7b9c933648d19df4dad720f2b7447802dd98c63c
최영준 <jjmullan24@gmail.com>
2025-07-31 10:13:39 +0900

    ✨ feat: Period 전용 기능 함수에서 날짜와 날짜간 비교하는 기능 함수 추가

--- src/utils/getDaysFromToday/getDaysFromToday.ts
+++ src/utils/getDaysFromToday/getDaysFromToday.ts
@@ -1,10 +1,24 @@
 import { parseISO, differenceInDays } from 'date-fns';
 
-export default function getDaysFromToday(dates: string[]) {
-  const today = new Date(); // 오늘 날짜
-  return dates.map(date => {
-    const parsedDate = parseISO(date); // 문자열 -> Date
+export default function getDaysFromToday({ startDate, endDates }: { startDate?: string; endDates: string[] }) {
+  let today: Date | string = new Date(); // 오늘 날짜
+
+  // 만약 startDate 가 있으면
+  if (startDate) {
+    today = parseISO(startDate);
+  }
+
+  return endDates.map(endDates => {
+    const parsedDate = parseISO(endDates); // 문자열 -> Date
     const diff = differenceInDays(parsedDate, today);
     return diff;
   });
+}
+
+export function getDayFromToday(startDate: string, endDate: string) {
+  const parsedStartDate = parseISO(startDate); // 문자열 -> Date
+  const parsedEndDate = parseISO(endDate); // 문자열 -> Date
+  const diff = differenceInDays(parsedEndDate, parsedStartDate);
+
+  return diff;
 }
