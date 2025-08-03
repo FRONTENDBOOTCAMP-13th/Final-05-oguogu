c12b66879f404b688e0b9c335089625460a9cde8
jaehyun0620 <zzz6451@naver.com>
2025-08-03 22:09:46 +0900

    💄 style: 자주하는질문 일부 스타일링 조정

--- src/app/board/qna/[_id]/page.tsx
+++ src/app/board/qna/[_id]/page.tsx
@@ -20,7 +20,7 @@ export default async function QnaDetailPage({ params }: QnaDetailPageProps) {
       <LinkHeader title="자주 묻는 질문" />
       <main className="px-4 py-4 min-h-[calc(100vh-48px)]">
         <div className="flex justify-between gap-2 border-b border-oguogu-gray-4 pb-1 mb-2 text-[16px] items-end ">
-          <div className="flex-shrink-0 w-[60px] text-oguogu-main ">{detailQna?.tag}</div>
+          <div className="flex-shrink-0 w-[80px] text-oguogu-main ">{detailQna?.tag}</div>
           <div className="truncate min-w-0 flex-1 text-oguogu-black ">{detailQna?.title}</div>
         </div>
         <div className="text-[12px] leading-[140%] mt-5">
